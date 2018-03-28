2a96895151516852749e01becb0bea0821e5a663
baihao <you@example.com>
2018-03-28 09:15:31 +0800

    计划书页面，页面的ajax部分完善

---
+++ js/prospect.js
@@ -0,0 +1,29 @@
+var prospect = new Vue({
+	el: '#Js-content',
+	data: {
+		alias: '',
+		plan: [
+			{
+				id: 1,
+		        ins: [{
+		          main: {},
+		          addon: {}
+		        }],
+				appl: {
+		          age: '',
+		          name: '',
+		          sex: ''
+		        },
+		        assu: {
+		          age: '',
+		          name: '',
+		          sex: ''
+		        }
+			}
+		]
+	},
+	methods: {
+		
+	}
+})
+		

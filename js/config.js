a55fa36ea4323a6eddc2a0a5b388018f0c8357b8
anloool <itqdkf06@ehuimeng.com>
2018-05-23 18:02:56 +0800

    团队管理修改

--- js/config.js
+++ js/config.js
@@ -5,12 +5,12 @@ var config = {
 	domain: 'https://www.luckyins.com'
 };
 
-     // var config = {
-     //     baseUrl: 'https://ts-www.luckyins.com/api/api/invoke',  //团队管理服务器路径
-     //     moduleURL: 'https://ts-www.luckyins.com/api/api/',
-     //     link: 'https://ts-www.luckyins.com/index/', // 外部链接路径[闪电增员、]
-     //     domain: 'https://ts-www.luckyins.com'
-     // };
+        // var config = {
+        //     baseUrl: 'https://ts-www.luckyins.com/api/api/invoke',  //团队管理服务器路径
+        //     moduleURL: 'https://ts-www.luckyins.com/api/api/',
+        //     link: 'https://ts-www.luckyins.com/index/', // 外部链接路径[闪电增员、]
+        //     domain: 'https://ts-www.luckyins.com'
+        // };
 
 
 /**
